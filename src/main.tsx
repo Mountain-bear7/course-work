bc2f3ef5b4fa2a25e6876d0ef69d49b7a57e86bf
Mountain-bear7 <doluxan010406@gmail.com>
2023-05-08 12:28:47 +0300

    added chakra ui and redux toolkit to the project

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App'
 import './index.css'
+import {ChakraProvider} from "@chakra-ui/react";
+import {Provider} from "react-redux";
+import {store} from "./store/store";
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+      <Provider store={store}>
+          <ChakraProvider>
+              <BrowserRouter>
+                  <div className='bg-[#131313]'>
+                      <App/>
+                  </div>
+              </BrowserRouter>
+          </ChakraProvider>
+      </Provider>
   </React.StrictMode>
 )
